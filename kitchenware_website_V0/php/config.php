cbaba9e054a0a9862850e3a292f82f4c812feec1
elofor <122603842+elofor@users.noreply.github.com>
2023-01-13 11:27:51 +0100

    Delete kitchenware_website_V0 directory

--- kitchenware_website_V0/php/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$host = "localhost";
-$user = "739527_2_1";
-$password = "SFzKmUPftTiQ";
-$dbname = "739527_2_1";
-
-$pdo = new PDO('mysql:host='. $host . '; dbname=' . $dbname . ';charset=utf8', $user, $password);
-$pdo->exec("set names utf8");
